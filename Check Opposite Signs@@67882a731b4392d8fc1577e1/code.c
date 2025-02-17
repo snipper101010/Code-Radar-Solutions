656307d3a766721f51770f3cdf46c0bb9d0bf9f6
Sakshi Saini <Sainilalita925@gmail.com>
2025-02-18 02:54:27 +0530

    Solution to Check Opposite Signs

--- Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -3,6 +3,6 @@ int main(){
     int num_1,num_2;
     scanf("%d %d",&num_1,&num_2);
    
-   (num_1^num_2)<0?printf("True"):printf("False");
+   (num_1*num_2)<0?printf("True"):printf("False");
    return 0;
 }
