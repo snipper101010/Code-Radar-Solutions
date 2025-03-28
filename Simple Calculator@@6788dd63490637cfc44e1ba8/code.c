3a44046464d047572df2803dd48faccca20d5c49
Sakshi Saini <Sainilalita925@gmail.com>
2025-03-28 23:20:01 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    double num1 ,num2, result;
+    char op;
+    scanf("%lf %lf %c",&num1,&num2,&op);
+    if(op== "+" )
+    {result = num1 + num2;
+    
+        printf(result);
+    }
+    return 0;
+
+
+}
+   
